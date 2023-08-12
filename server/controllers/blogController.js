916435a24d733cc3bc33f0e9a6795bbedec46e9d
Joynal Abedin <joynal.9933@gmail.com>
2023-08-12 21:38:41 +0600

    category controller done

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,7 +1,13 @@
+import blogModel from "../models/blogModel.js";
 class BlogController
 {
     static getAllBlogs = async(req, res) => {
-        res.send("get All blogs");
+        try{
+            const fetchAllBlog = await blogModel.find({});
+            return res.status(400).json(fetchAllBlog);
+        }catch(error){
+            return res.status(400).json({});
+        }
     }
     static addNewBlog = async(req, res) => {
         res.send("Add new blog");
--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,10 +1,40 @@
+import categoryModel from "../models/categoryModel.js";
 class CategoryController
 {
     static getAllCategories = async(req, res) => {
-        res.send("get All Categories");
+        try{
+            const fatchAllCategories = await categoryModel.find({});
+            return res.status(200).json(fatchAllCategories);
+        }catch(error){
+            return res.status(400).json({ message: error.message });
+        }
     }
     static addNewCategory = async(req, res) => {
-        res.send("Add new Category");
+        const { title } = req.body;
+        try{
+            if(title){
+
+                const isCategory = await categoryModel.findOne({title: title});
+
+                if(isCategory){
+                    return res.status(400).json({ message: `Category is already saved previously` });
+                }
+
+                const newCategory = new categoryModel({
+                    title,
+                });
+                const savedCategory = await newCategory.save();
+
+                if(savedCategory){
+                    return res.status(200).json({ message: `Category is saved` });
+                }
+
+            }else{
+                return res.status(400).json({ message: `All fields are required` });
+            }
+        }catch(error){
+            return res.status(400).json({ message: error.message });
+        }
     }
 }
 
